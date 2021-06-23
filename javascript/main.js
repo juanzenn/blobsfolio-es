5fcc3a429b00ce8c72ce6206bd55c6b44d007064
Juan Alvarez <juanandres140299@gmail.com>
2021-06-23 13:03:02 -0400

    Final version 1

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,25 @@
 const projects = document.getElementsByClassName('project-content')
 const projectsDescriptions = document.getElementsByClassName('description')
-const projectImages = document.getElementsByClassName('image')
+const projectLinearBg = document.getElementsByClassName('linear-bg')
 
 for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener('click', event => {
     toggle(event, projectsDescriptions, 'project-description')
-    toggle(event, projectImages,'project-image')
+    toggle(event, projectLinearBg, 'project-linear-bg')
   })
 }
 
 function toggle(event, items, toggleClass) {
   const index = parseInt(event.currentTarget.dataset.project)
   if (items[index].classList.contains(toggleClass)) {
+    items[index].style.animation = 'fadeIn 200ms linear forwards'
     items[index].classList.add(`${toggleClass}--active`)
-    items[index].classList.remove(`${toggleClass}`)
+    items[index].classList.remove(toggleClass)
     return
   }
-  items[index].classList.remove(`${toggleClass}--active`)
-  items[index].classList.add(`${toggleClass}`)
+  items[index].style.animation = 'fadeOut 300ms linear forwards'
+  setTimeout(function () {
+    items[index].classList.remove(`${toggleClass}--active`)
+    items[index].classList.add(toggleClass)
+  }, 300)
 }
